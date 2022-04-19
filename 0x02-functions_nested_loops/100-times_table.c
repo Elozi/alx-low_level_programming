82b6dbc6becf8e19aa24ebb5c1a65f6f21a02171
Elozi <abbyjamezzz@gmail.com>
2022-04-19 09:43:04 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,11 +33,12 @@ void print_times_table(int n)
 					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
+				{
 					_putchar((prod / 10) + '0');
-			}
-			_putchar((prod % 10) + '0');
+			        }
+			        _putchar((prod % 10) + '0');
+		        }
+		        _putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
 }
