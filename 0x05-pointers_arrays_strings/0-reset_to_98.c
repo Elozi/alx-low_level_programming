31cfaad18d393a78f85f648a284bfe90ea37b6bc
Elozi <abbyjamezzz@gmail.com>
2022-04-26 03:09:08 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
- /**
-  * reset_to_98 - updates the value *n points to.
-  * @n: the number to modify.
-  */
+/**
+ * reset_to_98 - updates the value *n points to.
+ * @n: the number to modify.
+ */
 
 void reset_to_98(int *n)
 {
