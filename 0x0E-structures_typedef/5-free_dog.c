aa82ff7002e26ba51968f143ff743f9c88229903
Elozi <abbyjamezzz@gmail.com>
2022-09-20 02:00:56 -0700

    tasks

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,8 @@
-#include "dog.h"
-#include <stdlib.h>
-/**
- * free_dog - frees memory of structure dogs
- * @d: pointer of structure
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void free_dog(dog_t *d)
-{
-	if (d != 0)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
-}
+-- creates the table unique_id
+-- table data: id INT with the default value 1, must be unique & name VARCHAR(256)
+-- should not fail if exists
+
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT DEFAULT 1 UNIQUE,
+       name VARCHAR(256)
+);
